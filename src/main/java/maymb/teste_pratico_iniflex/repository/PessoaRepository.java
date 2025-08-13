2b643353ae50cc96d9574df185abd5216c05aa12
MayaraMB <maybastos2021@gmail.com>
2025-08-13 10:16:44 -0300

    FEATURE: add interface respositorio e classe service para Pessoa

--- src/main/java/maymb/teste_pratico_iniflex/repository/PessoaRepository.java
+++ src/main/java/maymb/teste_pratico_iniflex/repository/PessoaRepository.java
@@ -1,4 +1,9 @@
 package maymb.teste_pratico_iniflex.repository;
 
-public class PessoaRepository {
+import maymb.teste_pratico_iniflex.entity.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 }
--- src/main/java/maymb/teste_pratico_iniflex/service/PessoaService.java
+++ src/main/java/maymb/teste_pratico_iniflex/service/PessoaService.java
@@ -1,4 +1,20 @@
 package maymb.teste_pratico_iniflex.service;
 
+import maymb.teste_pratico_iniflex.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public PessoaService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public void salvarPessoaNoBanco(){
+
+    }
 }
